4ebcc1617445184c7d8b2a062439e09c5e743d6b
Osamaomera <osama.omera68@gmail.com>
2024-06-09 21:49:44 +0300

    first

--- Jenkins Shared Library/deployOnOpenShift.groovy
+++ Jenkins Shared Library/deployOnOpenShift.groovy
@@ -7,11 +7,11 @@ def call(String OpenShiftCredentialsID, String openshiftClusterurl, String opens
     sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
     // login to OpenShift Cluster via cluster url & service account token
-    withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
-            sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
-            sh "oc project ${openshiftProject}"
-            sh "oc apply -f ."
-    }
+//     withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
+//             sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
+//             sh "oc project ${openshiftProject}"
+//             sh "oc apply -f ."
+//     }
 
     // login to OpenShift Cluster via KubeConfig file
     //withCredentials([file(credentialsId: 'OpenShiftCredentialsID', variable: 'KUBECONFIG_FILE')]) {
